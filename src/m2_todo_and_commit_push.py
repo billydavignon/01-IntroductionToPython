d586f2d22492d4e08adb47d3fc3b43be72f41a45
Billy Davignon <Davignwh@Rose-Hulman.edu>
2019-01-17 14:30:06 -0500

    1st todo done. 2nd todo is done. 3rd todo is done. 4th todo is done.

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -72,12 +72,15 @@ print("ram")
 
 ###############################################################################
 #
-# TODO: 4.
+# Done: 4.
 #   Add yet another   print   statement, putting it below this comment.
 #   This one should print the *product* of 3,607 and 34,227.
 #   Let the computer do the arithmetic for you (no calculators!).
 #   You do NOT have to use strings for this, so no quotation marks!
 #
+import math
+answer=3607*34227
+print(answer)
 #   TEST your code by re-running this module, then asking someone
 #   whom you trust:
 #      What number did your   print   display for this TO-DO?
